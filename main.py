4ddacd4b966f85d8193860e70c1daf1e348daa63
ARTEK <arteks96@gmail.com>
2020-11-01 20:33:28 +0100

    Json

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import subprocess
 import time
-#subprocess.call(r'net use m: \\192.168.16.175\nowyks /user:zps zps', shell=True)
 import configparser
+import json
+import socket
 
-
-
-#ini = open('./config.ini','r')
-#config =
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
 
 config = configparser.RawConfigParser()
 config.read('./config.ini')
@@ -14,17 +13,51 @@ litera=config.get('KONFIGURACJA','litera')
 sciezka=config.get('KONFIGURACJA','sciezka')
 login=config.get('KONFIGURACJA','login')
 login=config.get('KONFIGURACJA','haslo')
-
-#config.close()
+a=[]
 
 while True:
     time.sleep(1)
+    f = open('config.json', 'r')
+    sciezki = json.load(f)
+    adres = ""
+
+    for y in sciezki['uzytkownicy']:
+        if y["adres_ip"] == host_ip:
+            adres = y["adres_ip"]
+            dyski = y["litery"]
+
+
+    for x in sciezki["sciezki"]:
+        a.append(x)
+
+
+#    print(a[1][1:])
+    f.close()
+
     if subprocess.call(rf'{litera}', shell=True) == 0:
      #print("Nic sie nie dzieje")
-        pass
+
+
+        if host_ip==adres:
+
+            for d in dyski:
+
+                    print(d)
+
+                    if subprocess.call(rf'{d}:', shell=True) == 0:
+                        pass
+                    else:
+                        subprocess.call(rf'net use {d}: {x[1:]}', shell=True)
+
+        else:
+            pass
+
+
+
     else:
         subprocess.call(rf'net use {litera} {sciezka}', shell=True)
         #print("Podpieto dysk")
+
 
 
 
